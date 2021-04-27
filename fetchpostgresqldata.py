e5bcffd093fad89e39e51683ca88e2f2148dcd85
delores.cetleh <delores.cetleh@gmail.com>
2021-04-27 11:22:21 +0800

    add select command for sql

--- fetchpostgresqldata.py
+++ fetchpostgresqldata.py
@@ -9,6 +9,13 @@ print("Open database successfully")
 # mydata = {"test": 123}
 # cur.execute('''insert into LORADATA (TIMESTAMP,DATA) values  (NOW(),"12345");''')
 # print("Table created successfully")
+cursor = conn.cursor()
+cursor.execute(
+    "select device_name,battery_level,received_at from device_status;")
+rows = cursor.fetchall()
+for row in rows:
+print 'id=', row[0], ',name=', row[1], ',pwd=', row[2], ',singal=', row[3], '\n'
+conn.close()
 
 conn.commit()
 conn.close()
