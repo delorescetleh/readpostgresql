a047a1cb9fa7b07fea266516a9ddc5ef52c3ee76
delores.cetleh <delores.cetleh@gmail.com>
2021-04-27 11:01:10 +0800

    set db name/password

--- fetchpostgresqldata.py
+++ fetchpostgresqldata.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
-
 import psycopg2
 
-conn = psycopg2.connect(database="testdb", user="postgres",
-                        password="pass123", host="127.0.0.1", port="5432")
+conn = psycopg2.connect(database="chirpstack_as_events", user="chirpstack_as_events",
+                        password="dbpassword", host="127.0.0.1", port="5432")
+print("Open database successfully")
 
-print(' Opened database successfully')
+# cur = conn.cursor()
+# mydata = {"test": 123}
+# cur.execute('''insert into LORADATA (TIMESTAMP,DATA) values  (NOW(),"12345");''')
+# print("Table created successfully")
+
+conn.commit()
+conn.close()
